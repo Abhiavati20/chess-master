3c145ed287b9f97d49883067b5402ec1f57517f2
Abhiavati20 <abhishekaa1112@gmail.com>
2025-01-20 22:39:11 +0530

    CHESS GAME DEPLOYED ON AWS

--- frontend/hooks/socket/useSocket.ts
+++ frontend/hooks/socket/useSocket.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
     useEffect(() => {
-        const ws = new WebSocket("ws://localhost:8080");
+        const ws = new WebSocket("ws://65.0.176.68:80");
         ws.onopen = () => {
             console.log("CONNECTED");
             setSocket(ws);
